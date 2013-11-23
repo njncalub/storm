8751d25af7556933611f306447097356b525e333
njncalub <njncalub@gmail.com>
2013-11-24 00:44:33 +0800

    updated urls

--- stormapp/urls.py
+++ stormapp/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.http import HttpResponseRedirect
 from stormapp.deadbodies.views import home_view, sample_map_view, report_dead_body, view_all_dead_body
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -15,4 +14,5 @@ urlpatterns += patterns('',
     url(r'^view_map/$', sample_map_view),
     url(r'^report/$', report_dead_body),
     url(r'^view_all/$', view_all_dead_body),
+    url(r'^$', lambda x: HttpResponseRedirect('/index/')),
 )
